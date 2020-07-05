10ca77827a8cb1145a86f11703245d5141df58f7
Yuniaty Lay <yuniatylay@gmail.com>
2020-07-05 21:58:35 +0800

    first commit

---
+++ database/migrations/2020_07_05_131630_create_artikel_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateArtikelTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('artikel', function (Blueprint $table) {
+            $table->string('judul', 50);
+            $table->string('isi', 255);
+            $table->string('slug', 50);
+            $table->string('tag', 10);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('artikel');
+    }
+}
